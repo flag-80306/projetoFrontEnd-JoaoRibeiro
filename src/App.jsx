85c58ed330a1e162db887131dbcd75b7cb58030e
João ribeiro <joaopfribeiro@hotmail.com>
2023-12-06 19:58:34 +0000

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<div className='navBar'>Navigation Bar</div>
+			<div className='homeImageTitle'>image project - The Travel Blog</div>
+			<div className='secondTitle'>title to latest</div>
+			<div>5 different destinations</div>
+			<div>mapa</div>
+		</>
+	);
+}
+
+export default App;
