09b0a42ac8cbdf29fe810d2056a8e1e4e5bf4584
João ribeiro <joaopfribeiro@hotmail.com>
2023-12-06 23:54:44 +0000

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
 function App() {
 	return (
 		<>
-			<div className='navBar'>Navigation Bar</div>
-			<div className='homeImageTitle'>image project - The Travel Blog</div>
-			<div className='secondTitle'>title to latest</div>
-			<div className='cardContaienr'>5 different destinations</div>
-			<div className='mapArea'>mapa</div>
+			<div className='navBar'>
+				<div>
+					<img src='./img/logo.png' alt='' />
+				</div>
+				<div>
+					<nav>Navigation Bar</nav>
+				</div>
+			</div>
+			<div className='homeImageTitle'>
+				<div>
+					<img src='./img/slide_2.jpg' alt='' />
+				</div>
+				<h1>image project - The Travel Blog</h1>
+			</div>
+			<div className='secondTitle'>Latest</div>
+			<div className='cardContainer'>
+				<div className='card'>
+					<img src='https://picsum.photos/id/237/200' alt='arroz' />
+					<h3>Amarante</h3>
+					<p>(icon) 13/10/2020</p>
+				</div>
+				<div className='card'>
+					<img src='https://picsum.photos/id/237/200' alt='arroz' />
+					<h3>Amarante</h3>
+					<p>(icon) 13/10/2020</p>
+				</div>
+				<div className='card'>
+					<img src='https://picsum.photos/id/237/200' alt='arroz' />
+					<h3>Amarante</h3>
+					<p>(icon) 13/10/2020</p>
+				</div>
+				<div className='card'>
+					<img src='https://picsum.photos/id/237/200' alt='arroz' />
+					<h3>Amarante</h3>
+					<p>(icon) 13/10/2020</p>
+				</div>
+				<div className='card'>
+					<img src='https://picsum.photos/id/237/200' alt='arroz' />
+					<h3>Amarante</h3>
+					<p>(icon) 13/10/2020</p>
+				</div>
+			</div>
+			<div className='mapArea'>
+				<p>mapa</p>
+				<img src='https://www.oficinadanet.com.br/media/post/27852/750/adicionar-parada.jpg' alt='imagem de mapa' />
+			</div>
 		</>
 	);
 }
