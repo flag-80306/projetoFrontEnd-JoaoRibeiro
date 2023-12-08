2ab2b92ec2ab4e3a86cb82faf66f96d93cc96324
João ribeiro <joaopfribeiro@hotmail.com>
2023-12-08 17:44:32 +0000

    css index css header css main title css cards with 3 option of link# css main menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,61 @@ function App() {
 		<>
 			<div className='homeImageTitle'>
 				<img src='../img/slide_2.JPG' alt='' />
-				<div className='homePageTitle'>The Travel Blog</div>
+				<div className='homePageTitle'>
+					<h1>The Travel Blog</h1>
+				</div>
 			</div>
 			<div className='secondTitle'>
 				<h2>Latest</h2>
 			</div>
 			<div className='cardContainer'>
 				<div className='card'>
-					<img src='https://picsum.photos/id/237/200' alt='arroz' />
-					<h3>Amarante</h3>
-					<p>(icon) 13/10/2020</p>
+					<a href='#'>
+						<img src='https://picsum.photos/id/237/200' alt='arroz' className='cardImage' />
+					</a>
+					<div className='cardText'>
+						<h3>Amarante</h3>
+						<p>(icon) 13/10/2020</p>
+					</div>
 				</div>
 				<div className='card'>
-					<img src='https://picsum.photos/id/237/200' alt='arroz' />
-					<h3>Amarante</h3>
-					<p>(icon) 13/10/2020</p>
+					<img src='https://picsum.photos/id/237/200' alt='arroz' className='cardImage' />
+					<div className='cardText'>
+						<a href='#' className='cardText'>
+							<h3>Amarante</h3>
+						</a>
+						<p>(icon) 13/10/2020</p>
+					</div>
 				</div>
 				<div className='card'>
-					<img src='https://picsum.photos/id/237/200' alt='arroz' />
-					<h3>Amarante</h3>
-					<p>(icon) 13/10/2020</p>
+					<a href='#'>
+						<img src='https://picsum.photos/id/237/200' alt='arroz' className='cardImage' />
+						<div className='cardText'>
+							<h3>Amarante</h3>
+							<p>(icon) 13/10/2020</p>
+						</div>
+					</a>
 				</div>
 				<div className='card'>
-					<img src='https://picsum.photos/id/237/200' alt='arroz' />
-					<h3>Amarante</h3>
-					<p>(icon) 13/10/2020</p>
+					<a href='#'>
+						<img src='https://picsum.photos/id/237/200' alt='arroz' className='cardImage' />
+						<div className='cardText'>
+							<h3>Amarante</h3>
+							<p>(icon) 13/10/2020</p>
+						</div>
+					</a>
 				</div>
 				<div className='card'>
-					<img src='https://picsum.photos/id/237/200' alt='arroz' />
-					<h3>Amarante</h3>
-					<p>(icon) 13/10/2020</p>
+					<a href='#'>
+						<img src='https://picsum.photos/id/237/200' alt='arroz' className='cardImage' />
+						<div className='cardText'>
+							<h3>Amarante</h3>
+							<p>(icon) 13/10/2020</p>
+						</div>
+					</a>
 				</div>
 			</div>
 			<div className='mapArea'>
-				<p>Mapa</p>
 				<img src='https://www.oficinadanet.com.br/media/post/27852/750/adicionar-parada.jpg' alt='imagem de mapa' />
 			</div>
 		</>
