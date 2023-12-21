d6dbaff64ebb80cce6e71e3ee6bda73121b4e676
João ribeiro <joaopfribeiro@hotmail.com>
2023-12-21 11:37:14 +0000

    adicionar responsive navbar (ainda a meio)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,10 @@ function NavBar() {
 			<div className='topHeader'></div>
 			<div className='navBar'>
 				<nav className='headerMenu'>
-					<div className='headerMenuLogo'>
-						<a href='./index.html'>
-							<img src='/img/logo.png' className='logoHome' id='logoHome' />
-						</a>
-					</div>
+					<a href='./index.html' className='headerMenuLogo'>
+						<img src='/img/logo.png' className='logoHome' id='logoHome' />
+					</a>
+
 					<div className='headerMenuOptions'>
 						<div className='mainMenu'>
 							<ul className='mainMenuList'>
@@ -37,7 +36,36 @@ function NavBar() {
 							</ul>
 						</div>
 					</div>
+					<div className='headerMenuMobile'>
+						<button onclick='mobileMenu()'>
+							<img className='menuMobileIcon' src='./img/mobileMenu/menu_white_36dp.svg' alt='mobile menu icon' />
+						</button>
+					</div>
 				</nav>
+			</div>
+			<div className='mobileMenu'>
+				<ul className='mainMobileMenuList'>
+					<li>
+						<Link href='/'>
+							<h3>Home</h3>
+						</Link>
+					</li>
+					<li>
+						<Link href='/Experiences'>
+							<h3>Experiences</h3>
+						</Link>
+					</li>
+					<li>
+						<Link href='/AboutUs'>
+							<h3>About us</h3>
+						</Link>
+					</li>
+					<li>
+						<Link href='/Contact'>
+							<h3>Contact Us</h3>
+						</Link>
+					</li>
+				</ul>
 			</div>
 		</>
 	);
