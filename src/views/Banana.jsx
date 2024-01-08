6ac2432f3d45e12f8d304551ac670f0992f9a93a
João ribeiro <joaopfribeiro@hotmail.com>
2024-01-08 23:26:20 +0000

    cardInfo

--- src/views/Banana.jsx
+++ src/views/Banana.jsx
@@ -1,14 +1,48 @@
-import TourContainer from '../components/TourContainer';
-import CardInfo from '../components/CardInfo';
+import { useEffect, useState } from 'react';
+import tourAPIService from '../services/tourAPIService.js';
+import Slide from '../components/Slide.jsx';
 
 function Banana({ params }) {
-	console.log(params.tour);
-	console.log(123);
+	const [tourDetails, setTourDetails] = useState({});
+
+	useEffect(function () {
+		(async function () {
+			const responseAPI = await tourAPIService.getToursData();
+			const findInfo = responseAPI.find(obj => {
+				return obj.nameID == params.tour;
+			});
+			setTourDetails(findInfo);
+			console.log(findInfo);
+		});
+	}, []);
+
+	console.log(tourDetails);
+
 	return (
 		<>
-			<div>
-				Escolhes-te o{params.tour}
-				{/* <CardInfo tourInfo={tour} /> */}
+			<div className='cardInfoTitle marginFifty'>
+				{/* <h2>Inside Marão</h2> */}
+				<h2>{params.name}</h2>
+				<p>
+					<img src='/img/schedule.svg' alt='Schedule icon' className='icon' />
+					Available all year
+				</p>
+				<p>(icon){tourDetails.date}</p>
+			</div>
+			<div className='CardSlide'>
+				{tourDetails.slideInfo?.map(item => (
+					<Slide key={item.id} services={item} />
+				))}
+			</div>
+			<div className='cardMap'>
+				<img src='https://www.oficinadanet.com.br/media/post/27852/750/adicionar-parada.jpg' alt='mapa' />
+
+				<p>mapa (precisamos de latitude e longitude para marcar cada um dos 5 pontos de atração)</p>
+			</div>
+			<div className='infoCard'>
+				<div className='cardDescription'>
+					<p>{tourDetails.description}</p>;
+				</div>
 			</div>
 		</>
 	);
